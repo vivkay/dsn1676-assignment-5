20e7033fa4397ab15d74aa9f885fc97fd4307f42
Vivienne Kay <vivkay8@gmail.com>
2015-02-23 19:49:37 -0500

    adds javascript append items and css
    removes decoration from ul element

--- js/main.js
+++ js/main.js
@@ -1,85 +1,17 @@
-
-var $add = $('#add');
-var $form = $('form');
+var $add = $('.add');
+var $adding = $('#add-item');
+var $form = $('.form');
+var $list = $('.list');
 
 $form.on('submit', function (e) {
     e.preventDefault();
-    var $div = $('<div>');
-    $div.addClass('item');
-    $('body').append($div);
+    var $li = $('<li>');
+    //$list.append($li);
+
+    var addValue = $add.val();
+    var $addH2 = $('<h2>').html($add.val());
+
+    $li.append($addH2);
+    $list.append($li);
 });
-
-
-
-
-
-
-
-
-$form.on('submit', function (e) {
-    //stop the form from doing what it normally does
-    e.preventDefault();
-    $circle.css('background-color', $theColor.val());
-});
-
-
-    $newSquare.on('click', function() {
-    var $div = $('<div>');
-    $div.addClass('square');
-    $('body').append($div);
-});
-
-
-// $('.circle').css('border', '3px solid black'); 
-
-var $circle = $('.circle');
-var $moveDownBtn = $('.move-down');
-var $form = $('form');
-var $theColor = $('#the-color');
-var $newSquare = $('.new-square');
-
-$moveDownBtn.on('click', function () {
-    var offset = $circle.offset();
-    $circle.css('top', offset.top + 10);
-});
-
-var $moveUpBtn = $('.move-up');
-
-$moveUpBtn.on('click', function () {
-    var offset = $circle.offset();
-    $circle.css('top', offset.top - 10);
-});
-
-var $moveLeftBtn = $('.move-left');
-
-$moveLeftBtn.on('click', function () {
-    var offset = $circle.offset();
-    $circle.css('left', offset.left - 10);
-});
-
-var $moveRightBtn = $('.move-right');
-
-$moveRightBtn.on('click', function () {
-    var offset = $circle.offset();
-    $circle.css('left', offset.left + 10);
-});
-
-// e is the event object. it tells us what element was clicked on, where it was clicked etc. you can also use an event object to stop default actions on items such as forms
-
-$form.on('submit', function (e) {
-    //stop the form from doing what it normally does
-    e.preventDefault();
-    $circle.css('background-color', $theColor.val());
-});
-
-// $('body') tells it to look at the body. The .append tells it to add whatever following element to the body.
-
-$newSquare.on('click', function() {
-    var $div = $('<div>');
-    $div.addClass('square');
-    $('body').append($div);
-});
-
-
-
 
