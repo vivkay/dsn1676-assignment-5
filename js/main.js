1efc23bbdddd5e0a036e90a70c167610948361db
Vivienne Kay <vivkay8@gmail.com>
2015-02-27 20:44:17 -0500

    tried adding list but to no avail!

--- js/main.js
+++ js/main.js
@@ -3,8 +3,34 @@ var $action = $('.action');
 var $list = $('.list');
 
 $form.on('submit', function () {
-   e.preventDefault();
+    e.preventDefault();
 
-var actionValue = $action.val();
-    console.log(actionValue);
+    var $actionH2 = $('<h2>').html($action.val())
+    console.log($actionValue);
+    
+    $li.append($actionH2);
+    $list.append($li);
 });
+  
+
+   
+// var $form = $('.form');
+// var $name = $('.name');
+// var $age = $('.age');
+// var $dinos = $('.dinos');
+
+// $form.on('submit', function (e) {
+//   e.preventDefault();
+
+// var $li = $('<li>');
+// var $nameH2 = $('<h2>').html($name.val());
+// var $ageP = $('<p>').html('Age: ' + $age.val() + ' million years old');
+// var $diet = $('[name="diet"]:checked')
+
+// $li.append($nameH2).append($ageP).addClass($diet.val());
+// $dinos.append($li);
+// });
+
+// $dinos.on('click', 'li', function () {
+//   $(this).toggleClass('js-highlight');
+// });
