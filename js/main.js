c025c8888e4e6dcef55d3387e2a3e91b5a36ef05
Vivienne Kay <vivkay8@gmail.com>
2015-02-27 20:30:33 -0500

    trying to add js to make form work (broken)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $form = $('.form');
+var $action = $('.action');
+var $list = $('.list');
 
 $form.on('submit', function () {
    e.preventDefault();
-});
 
+var actionValue = $action.val();
+    console.log(actionValue);
+});
